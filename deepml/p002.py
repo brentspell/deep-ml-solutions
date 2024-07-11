ff40d59e3b98d69b4378abd8c90308ce39c6b5f0
Brent M. Spell <bspell1@gmail.com>
2024-07-11 16:13:35 -0400

    problem 2

---
+++ deepml/p002.py
@@ -0,0 +1,15 @@
+def transpose_matrix(a: list[list[int | float]]) -> list[list[int | float]]:
+    if len(a) == 0:
+        return []
+
+    return [[a[j][i] for j in range(len(a))] for i in range(len(a[0]))]
+
+
+def test_transpose_matrix() -> None:
+    # empty/degenerate matrix
+    assert transpose_matrix([]) == []
+    assert transpose_matrix([[]]) == []
+
+    # valid matrix
+    a: list[list[int | float]] = [[1, 2, 3], [4, 5, 6]]
+    assert transpose_matrix(a) == [[1, 4], [2, 5], [3, 6]]
